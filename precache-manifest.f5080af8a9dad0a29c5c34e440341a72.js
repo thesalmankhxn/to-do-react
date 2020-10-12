cbd983e180164914d01680ec8ead6d23fad2e581
iamSK77 <sk77.mobilefix@gmail.com>
2020-10-13 01:43:00 +0530

    Updates

---
+++ precache-manifest.f5080af8a9dad0a29c5c34e440341a72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383908a1ea065a7e8aaee73c611aedb1",
+    "url": "/to-do-react/index.html"
+  },
+  {
+    "revision": "3d89da760c8e4fbbc932",
+    "url": "/to-do-react/static/css/main.fa1e9343.chunk.css"
+  },
+  {
+    "revision": "99b673a551656574b7a0",
+    "url": "/to-do-react/static/js/2.6e33769e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-react/static/js/2.6e33769e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d89da760c8e4fbbc932",
+    "url": "/to-do-react/static/js/main.ba6bf23a.chunk.js"
+  },
+  {
+    "revision": "8241a85c2ebbdf5126fe",
+    "url": "/to-do-react/static/js/runtime-main.49965aa0.js"
+  }
+]);
